0be566194bceb3e03c7332fa52baf22f848bf809
sreejith87 <sreejith.darmatek@gmail.com>
2019-01-07 21:20:21 +0800

    eight

--- www/SreeSamplePlugin.js
+++ www/SreeSamplePlugin.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+module.exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'SreeSamplePlugin', 'coolMethod', [arg0]);
 };
-exports.Addition = function (arg0, success, error) {
+module.exports.Addition = function (arg0, success, error) {
     exec(success, error, 'SreeSamplePlugin', 'Addition', [arg0]);
 };
